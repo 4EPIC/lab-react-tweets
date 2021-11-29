73e7f9625338eebf224e95885ef92e21a93fb3df
Eric Esteban <eric_7@hotmail.com>
2021-11-29 17:55:26 +0100

    Lab Done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={tweetsArray[0]} />
+      {tweetsArray.map((item, i) => (
+        <Tweet tweet={item} key={i} />
+      ))}
     </div>
   );
 }
